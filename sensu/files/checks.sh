df9b82dc2f02644a70926477642f558be2e37057
Kurt Bendlin <kurt@bendlin.de>
2016-04-14 21:08:50 +0200

    query for the results of this clients

--- sensu/files/checks.sh
+++ sensu/files/checks.sh
@@ -1,3 +1,3 @@
 {%- from "sensu/pillar_map.jinja" import sensu with context %}
 #!/bin/bash
-curl -s http://access:granted@{{ sensu.rabbitmq.host }}:4567/clients/{{ grains['fqdn'] }}-{{ grains['machine_id'] }}
+curl -s http://access:granted@{{ sensu.rabbitmq.host }}:4567/results/{{ grains['fqdn'] }}-{{ grains['machine_id'] }}
